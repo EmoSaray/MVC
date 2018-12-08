8ac41b79c82f3baf376509a7a5931ebfdd4e56da
Emilian Saraydarov <emilian.saraydarov@gmail.com>
2018-12-08 15:28:38 +0200

    Add project files.

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewsModels;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random( )
+        {
+            Movie movie = new Movie() { Name = "Алабала" };
+
+            var customers = new List<Customer>()
+            {
+                new Customer(){ Name = "Мартин "},
+                new Customer(){ Name = "Стефан "}
+            };
+
+            var viewModel = new RandomMoiveViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+
+            return View(viewModel);
+            //return View(movie);
+            //return Content("Здраей, Стефан");
+            //return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("БРАВО! ID=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format( "pageIndex={0}&sortBy={1}",pageIndex, sortBy ));
+        }
+    }
+}
---
+++ WebApplication1/ViewsModels/RandomMoiveViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewsModels
+{
+    public class RandomMoiveViewModel
+    {
+        public Models.Movie Movie { get; set; }
+        public List<Models.Customer> Customers { get; set; }
+    }
+}
